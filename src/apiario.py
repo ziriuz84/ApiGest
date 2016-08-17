372d617d9dbb86b54b098614f972ee28dca0e793
ziriuz84 <ziriuz84@gmail.com>
2016-08-17 21:35:01 +0200

    Aggiunta la classe Apiario

---
+++ src/apiario.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+class Apiario:
+
+    def __init__(self):
+        self.ID = 0
+        self.Nome = ""
+        self.Posizione = ""
+
+    def Aggiungi(self):
+        return
+
+    def Modifica(self):
+        return
+
+    def Elimina(self):
+        return
+
+    def InterfacciaAggiungi(self):
+        return
