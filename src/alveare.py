ea49861442d6ffab4c546f426c4844818f2a5459
ziriuz84 <ziriuz84@gmail.com>
2016-08-28 07:32:57 +0200

    Aggiunta classe Alveare con relativa struttura

---
+++ src/alveare.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class Alveare:
+    def __init__(self):
+        return
+
+    def Aggiungi(self):
+        return
+
+    def Modifica(self):
+        return
+
+    def Elimina(self):
+        return
+
+    def Elenca(self):
+        return
